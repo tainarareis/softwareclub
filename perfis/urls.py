9699cc54c48f4b26be53a326cb13c9808d9a064a
tainarareis <tainara.s.r@hotmail.com>
2016-06-13 17:42:34 -0300

    Improving design

--- perfis/urls.py
+++ perfis/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from perfis import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^homepage$', views.homepage, name='homepage'),
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^index/$', views.index, name='index'),
     #url(r'^busca$', views.busca, name='busca'),
     url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
 	url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar')
