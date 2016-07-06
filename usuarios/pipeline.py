ba885687070acd99c0f8400cc51bc6d34f42c442
tainarareis <tainara.s.r@hotmail.com>
2016-07-06 20:34:48 -0300

    Configuring pipeline from django-social-auth

---
+++ usuarios/pipeline.py
@@ -0,0 +1,5 @@
+from .models import UserProfile
+
+
+def salvar_perfil(backend, user, response, *args, **kwargs):
+    print(user)
--- usuarios/views.py
+++ usuarios/views.py
@@ -7,6 +7,13 @@ from django.contrib.auth.models import User
 from perfis.models import Perfil
 from django.template.context import RequestContext
 
+def home(self, request):
+   context = RequestContext(request,
+                           {'request': request,
+                            'user': request.user})
+   return render_to_response('home.html',
+                             context_instance=context)
+
 class RegistrarUsuarioView(View):
 
     template_name = 'registrar.html'
